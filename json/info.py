0aac776d1ad17d2583a986ac3a4c8a97aef3f6f7
Mathias Felde Haugen <111960714+matfelhau@users.noreply.github.com>
2022-10-23 20:12:08 +0200

    json

--- json/info.py
+++ json/info.py
@@ -1,12 +1,26 @@
 #Work in progress
 import json
 
-f = open('personas.json', 'r')
+f = open('json\personas.json', 'r')
 
 #JSON:
-data = json.load(f)
+personer = json.load(f)
 
-for i in data[0]["navn"]:
-  print(i)
+#Viser hva slags keys vi kan hente ut om personene:
+#Vi spesifisere person 1, men ettersom alle personene har samme key-values så reflekterer det alle, som vi har det nå.
+for keys in personer[0]:
+  print(keys)
+
+#Henter ut Fornavn og etternavn til Person 2.
+print(personer[1]['firstName'], personer[1]['lastName'])
+
+#Henter ut all dataen vi har om alle fra personas.json filen.
+print(personer)
+
+#Henter all dataen om Karl(Person 2)
+Karl = personer[1]
+print(Karl)
 
 f.close()
+
+
