bf4187afe53ee35f72fbc9c364df41e5784e4527
eirikjg46 <eirikjg@hiof.no>
2022-10-23 11:43:39 +0200

    Changes

--- base_layout.py
+++ base_layout.py
@@ -21,6 +21,19 @@ class BaseLayout(tk.Tk):
 
     def view_cars(self):
         print("Nå ser du på biler. Wow. De er veldig fine.")
+        # Toplevel object which will
+        # be treated as a new window
+        newWindow = Toplevel(self)
+    
+        # sets the title of the
+        # Toplevel widget
+        newWindow.title("New Window")
+    
+        # sets the geometry of toplevel
+        newWindow.geometry("200x200")
+    
+        # A Label widget to show in toplevel
+        Label(newWindow, text ="This is a new window").pack()
 
     def lend_your_car(self):
         print("Så hyggelig av deg å låne bort din egen bil.")
